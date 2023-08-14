923b3dc914105a33789d13df69067699215618cc
ahmadHajouj <ahmad1416h@gmail.com>
2023-08-14 08:45:24 +0300

    try to solve access problem

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,7 +3,20 @@ import axios, { CanceledError } from "axios";
 export default axios.create({
   baseURL: "https://credit-debit-api.vercel.app",
 });
+axios.interceptors.response.use(null, (error) => {
+  axios.defaults.withCredentials = true;
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
 
+  if (!expectedError) {
+    logger.log(error);
+    toast.error("An unexpected error occurrred.");
+  }
+
+  return Promise.reject(error);
+});
 function setJwt(jwt) {
   axios.defaults.headers.common["x-auth-token"] = jwt;
 }
